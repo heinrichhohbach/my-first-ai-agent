64379ac2c97f4d489807b2aa7906bd1d62ebef2a
Heinrich Hohbach <heinrich.hohbach@gmail.com>
2025-07-29 20:10:02 +0100

    Add function declaration for get_files_info and update main function to utilize it

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from functions.get_files_info import schema_get_files_info
 
 def main():
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     args = sys.argv[1:]
@@ -32,12 +41,26 @@ def main():
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
 
-    print(response.text)
+    if response.candidates[0].content.parts:
+        for part in response.candidates[0].content.parts:
+            if hasattr(part, 'function_call'):
+                print(f"Calling function: {part.function_call.name}({part.function_call.args})")
+            elif hasattr(part, 'text') and part.text:
+                print(part.text)
+    else:
+        print("No response parts found")
 
 def generate_ai_response(api_key, input_prompt, messages, system_prompt):
+    available_functions = genai.types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
     client = genai.Client(api_key=api_key)
     messages.append(genai.types.Content(role="user", parts=[genai.types.Part(text=input_prompt)]))
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=genai.types.GenerateContentConfig(system_instruction=system_prompt),)
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=genai.types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+),)
     return response, messages
 
 
